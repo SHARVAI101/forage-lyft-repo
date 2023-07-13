9c7c99a6d320ed84ec55e36e8e21d9372b9b8a5f
Sharvai <sharvai101@gmail.com>
2023-07-13 14:59:11 -0400

    updated architecture

---
+++ battery/nubbin_battery.py
@@ -0,0 +1,12 @@
+from .battery import Battery
+from dateutil.relativedelta import relativedelta
+
+class NubbinBattery(Battery):
+    def __init__(self, current_date, last_service_date):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+
+    def needs_service(self):
+        if relativedelta(self.current_date, self.last_service_date).years > 4:
+            return True
+        return False
